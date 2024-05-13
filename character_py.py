51f57fba431be120e5dcc64e34c7e3ca53aeb639
Chevie <Jeffllontop97@gmail.com>
2024-05-13 12:12:43 -0400

    primer commit battle text

---
+++ character_py.py
@@ -0,0 +1,12 @@
+class Character:
+    race = "Human"
+
+    def __init__(self, name: str, health: int, damage: int):
+        self.name = name
+        self.health = health
+        self.health_max = health
+        self.damage = damage
+
+    def attack(self, target) -> None:
+        target.health -= self.damage
+        target.health = max(target.health, 0)
